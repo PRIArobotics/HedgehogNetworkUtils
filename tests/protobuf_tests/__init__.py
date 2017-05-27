60023c45f9bcc9a83d6a1bb8ce9cced91e96516b
SillyFreak <sillyfreak.ck@gmail.com>
2017-05-27 20:25:02 +0200

    add TestKind to allow for more complex test cases

--- tests/protobuf_tests/__init__.py
+++ tests/protobuf_tests/__init__.py
@@ -1,6 +1,7 @@
 from hedgehog.utils.protobuf import ContainerMessage, Message
 from .proto import test_pb2
 
+from .proto.test_pb2 import DEFAULT, ALTERNATIVE
 
 Msg1 = ContainerMessage(test_pb2.TestMessage1)
 Msg2 = ContainerMessage(test_pb2.TestMessage2)
@@ -9,13 +10,16 @@ Msg2 = ContainerMessage(test_pb2.TestMessage2)
 @Msg1.register(test_pb2.Test, 'test')
 @Msg2.register(test_pb2.Test, 'test')
 class Test(Message):
-    def __init__(self, field):
+    def __init__(self, kind, field):
+        self.kind = kind
         self.field = field
 
     @classmethod
     def _parse(cls, msg):
+        kind = msg.kind
         field = msg.field
-        return cls(field)
+        return cls(kind, field)
 
     def _serialize(self, msg):
+        msg.kind = self.kind
         msg.field = self.field
--- tests/test_protobuf.py
+++ tests/test_protobuf.py
@@ -4,13 +4,13 @@ import protobuf_tests
 
 class ProtobufTests(unittest.TestCase):
     def test_packed_message(self):
-        old = protobuf_tests.Test(1)
+        old = protobuf_tests.Test(protobuf_tests.DEFAULT, 1)
         new = protobuf_tests.Msg1.parse(protobuf_tests.Msg1.serialize(old))
         self.assertEqual(new, old)
         new = protobuf_tests.Msg2.parse(protobuf_tests.Msg2.serialize(old))
         self.assertEqual(new, old)
 
     def test_simple_message(self):
-        old = protobuf_tests.Test(1)
+        old = protobuf_tests.Test(protobuf_tests.DEFAULT, 1)
         new = protobuf_tests.Test.parse(old.serialize())
         self.assertEqual(new, old)
