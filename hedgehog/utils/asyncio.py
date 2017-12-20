fcafff21691121366350e65700bf45b420d3dae4
SillyFreak <sillyfreak.ck@gmail.com>
2017-12-20 15:34:24 +0100

    improve actor code, especially error handling

--- hedgehog/utils/asyncio.py
+++ hedgehog/utils/asyncio.py
@@ -1,37 +1,63 @@
 import asyncio
 
+__all__ = ['pipe', 'Actor', 'ActorException']
+
 
 def pipe():
     class _End(object):
         def __init__(self, r, w):
-            self.r = r
-            self.w = w
+            self._r = r
+            self._w = w
 
         async def send(self, msg):
-            await self.w.put(msg)
+            await self._w.put(msg)
 
         async def recv(self):
-            return await self.r.get()
+            return await self._r.get()
 
     a = asyncio.Queue()
     b = asyncio.Queue()
     return _End(a, b), _End(b, a)
 
 
+class ActorException(Exception):
+    pass
+
+
 class Actor(object):
     class Task(object):
-        def __init__(self, run, *args, **kwargs):
-            self.cmd_pipe, cmd_pipe = pipe()
-            self.evt_pipe, evt_pipe = pipe()
+        def __init__(self, run):
+            self.cmd_pipe, self._cmd_pipe = pipe()
+            self.evt_pipe, self._evt_pipe = pipe()
+
+            self._run = run
+            self._future = None
+            self._state = None
+
+        async def start(self):
+            assert self._state is None
 
             async def _run():
-                await run(cmd_pipe, evt_pipe, *args, **kwargs)
-                await evt_pipe.send(b'$TERM')
+                try:
+                    await self._run(self._cmd_pipe, self._evt_pipe)
+                finally:
+                    await self._evt_pipe.send(b'$TERM')
 
             self._future = asyncio.ensure_future(_run())
             self._state = 'running'
 
+            start = await self.evt_pipe.recv()
+            if start == b'$TERM':
+                # run terminated during initialization.
+                # Initialization may have raised an exception, in that case reraise it:
+                await self._future
+                # otherwise, run did not send any events, including b'$START'; fall-through
+            if start != b'$START':
+                raise ActorException("run() must send b'$START' to signal actor initialization!")
+
         async def destroy(self, block=True):
+            assert self._state is not None
+
             if self._state == 'running':
                 await self.cmd_pipe.send(b'$TERM')
                 self._state = 'destroyed'
@@ -40,16 +66,22 @@ class Actor(object):
                 while (await self.evt_pipe.recv()) != b'$TERM':
                     pass
                 self._state = 'terminated'
+                await self._future
 
     async def __aenter__(self):
         self._task = Actor.Task(self.run)
-        start = await self._task.evt_pipe.recv()
-        if start != b'$START':
-            raise ValueError("run() must send b'$START' to signal actor initialization!")
+        try:
+            await self._task.start()
+        except Exception:
+            self._task = None
+            raise
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self._task.destroy()
+        try:
+            await self._task.destroy()
+        finally:
+            self._task = None
 
     @property
     def cmd_pipe(self):
@@ -60,4 +92,4 @@ class Actor(object):
         return self._task.evt_pipe
 
     async def run(self, cmd_pipe, evt_pipe):
-        raise NotImplementedError()
+        raise NotImplementedError()  # pragma: no cover
--- tests/test_asyncio.py
+++ tests/test_asyncio.py
@@ -1,37 +1,111 @@
 import pytest
-import asyncio
+from aiostream import stream
+from contextlib import contextmanager
+
+from hedgehog.utils.asyncio import pipe, Actor, ActorException
 
 
-class TestAsyncActor(object):
-    @pytest.mark.asyncio
-    async def test_async_actor(self):
-        import hedgehog.utils.asyncio
+@contextmanager
+def assert_actor_cleanup(actor):
+    try:
+        yield actor
+    finally:
+        with pytest.raises(AttributeError):
+            actor.cmd_pipe
 
-        class MyActor(hedgehog.utils.asyncio.Actor):
-            async def command(self, cmd):
-                await t.cmd_pipe.send((b'CMD', cmd))
 
-            async def run(self, cmd_pipe, evt_pipe):
-                await evt_pipe.send(b'$START')
+@pytest.mark.asyncio
+async def test_pipe():
+    a, b = pipe()
 
-                # while True:
-                #     cmd = await cmd_pipe.recv()
-                #     if cmd == b'$TERM':
-                #         break
-                #
-                #     cmd, *payload = cmd
-                #     if cmd == b'CMD':
-                #         print(*payload)
+    await a.send("foo")
+    assert await b.recv() == "foo"
 
-                cmd = await cmd_pipe.recv()
-                assert cmd == (b'CMD', "hello")
 
-                cmd = await cmd_pipe.recv()
-                assert cmd == b'$TERM'
+@pytest.mark.asyncio
+async def test_actor():
+    class MyActor(Actor):
+        def __init__(self, greeting):
+            self.greeting = greeting
 
-        try:
-            async with MyActor() as t:
-                await t.command("hello")
-        except asyncio.CancelledError:
-            import traceback
-            traceback.print_exc()
+        async def greet(self, msg):
+            await self.cmd_pipe.send((b'GREET', msg))
+            return await self.cmd_pipe.recv()
+
+        async def agreet(self, msg):
+            await self.cmd_pipe.send((b'AGREET', msg))
+
+        async def run(self, cmd_pipe, evt_pipe):
+            await evt_pipe.send(b'$START')
+
+            expected = [(b'GREET', "world"), (b'AGREET', "world"), b'$TERM']
+
+            async with stream.count().stream() as streamer:
+                i = -1
+                async for i in streamer:
+                    cmd = await cmd_pipe.recv()
+                    assert cmd == expected[i]
+
+                    if cmd == b'$TERM':
+                        break
+
+                    cmd, *payload = cmd
+
+                    if cmd == b'GREET':
+                        msg, = payload
+                        await cmd_pipe.send(f"{self.greeting} {msg}")
+                    elif cmd == b'AGREET':
+                        msg, = payload
+                        await evt_pipe.send((b'AGREET', f"{self.greeting} {msg}"))
+                assert i == len(expected) - 1
+            # this happens when terminating the actor, so is always ignored
+            await evt_pipe.send((b'IGNORED',))
+
+    with assert_actor_cleanup(MyActor("hello")) as a:
+        async with a:
+            assert await a.greet("world") == "hello world"
+            await a.agreet("world")
+            assert await a.evt_pipe.recv() == (b'AGREET', "hello world")
+
+
+@pytest.mark.asyncio
+async def test_faulty_actor():
+    class FaultyActor(Actor):
+        async def run(self, cmd_pipe, evt_pipe):
+            pass
+
+    with pytest.raises(ActorException):
+        with assert_actor_cleanup(FaultyActor()) as a:
+            async with a:
+                pass
+
+
+@pytest.mark.asyncio
+async def test_failing_actor():
+    class MyException(Exception):
+        pass
+
+    class FailingActor(Actor):
+        async def run(self, cmd_pipe, evt_pipe):
+            await evt_pipe.send(b'$START')
+            raise MyException()
+
+    with pytest.raises(MyException):
+        with assert_actor_cleanup(FailingActor()) as a:
+            async with a:
+                pass
+
+
+@pytest.mark.asyncio
+async def test_init_failing_actor():
+    class MyException(Exception):
+        pass
+
+    class InitFailingActor(Actor):
+        async def run(self, cmd_pipe, evt_pipe):
+            raise MyException()
+
+    with pytest.raises(MyException):
+        with assert_actor_cleanup(InitFailingActor()) as a:
+            async with a:
+                pass
