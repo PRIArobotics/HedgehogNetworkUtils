4b0bf69f829af42670550f7879bc18e084cab558
SillyFreak <sillyfreak.ck@gmail.com>
2018-02-05 23:26:38 +0100

    prepare release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='hedgehog-utils',
-    version='0.4.2',
+    version='0.5.0',
     description='Utilities shared between Hedgehog projects',
     long_description=long_description,
     url="https://github.com/PRIArobotics/HedgehogUtils",
