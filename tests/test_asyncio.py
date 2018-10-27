ab024ab473e345ca26773fd9da6b5accdd48bb16
SillyFreak <sillyfreak.ck@gmail.com>
2018-10-27 14:18:25 +0200

    remove pipe and actor code

--- tests/test_asyncio.py
+++ tests/test_asyncio.py
@@ -3,11 +3,9 @@ from hedgehog.utils.test_utils import event_loop, assertTimeout, assertImmediate
 
 import asyncio
 import itertools
-from aiostream import stream, pipe as p_
-from contextlib import contextmanager
+from aiostream import stream, pipe
 
-from hedgehog.utils.asyncio import repeat_func, repeat_func_eof, stream_from_queue, pipe
-from hedgehog.utils.asyncio import Actor, ActorException
+from hedgehog.utils.asyncio import repeat_func, repeat_func_eof, stream_from_queue
 
 
 # Pytest fixtures
@@ -20,15 +18,6 @@ async def assert_stream(expected, _stream):
         async for i, item in streamer:
             assert item == expected[i]
         assert i == len(expected) - 1
-
-
-@contextmanager
-def assert_actor_cleanup(actor):
-    try:
-        yield actor
-    finally:
-        with pytest.raises(AttributeError):
-            actor.cmd_pipe
 
 
 @pytest.mark.asyncio
@@ -58,7 +47,7 @@ async def test_stream_from_queue():
 
         await assert_stream(
             [0, 1, 2],
-            (stream_from_queue(queue) | p_.action(put_next))[:3])
+            (stream_from_queue(queue) | pipe.action(put_next))[:3])
 
 
 @pytest.mark.asyncio
@@ -73,163 +62,4 @@ async def test_stream_from_queue_eof():
 
         await assert_stream(
             [3, 2, 1, 0],
-            stream_from_queue(queue, EOF, use_is=True) | p_.action(put_next))
-
-
-@pytest.mark.asyncio
-async def test_pipe():
-    a, b = pipe()
-
-    task = asyncio.ensure_future(b.recv())
-    await assertTimeout(task, 1, shield=True)
-    with assertImmediate():
-        await a.send("foo")
-        assert await task == "foo"
-
-
-@pytest.mark.asyncio
-async def test_repeat_func_eof_task():
-    a, b = pipe()
-
-    async def run():
-        async with stream.enumerate(repeat_func_eof(b.recv, 10)).stream() as streamer:
-            i = -1
-            async for i, msg in streamer:
-                assert msg == i
-            assert i == 9
-
-    with assertImmediate():
-        task = asyncio.ensure_future(run())
-        for i in range(11):
-            await a.send(i)
-        await task
-
-
-@pytest.mark.asyncio
-async def test_actor():
-    class MyActor(Actor):
-        def __init__(self, greeting):
-            super(MyActor, self).__init__()
-            self.greeting = greeting
-
-        async def greet(self, msg):
-            await self.cmd_pipe.send((b'GREET', msg))
-            return await self.cmd_pipe.recv()
-
-        async def agreet(self, msg):
-            await self.cmd_pipe.send((b'AGREET', msg))
-
-        async def run(self, cmd_pipe, evt_pipe):
-            await evt_pipe.send(b'$START')
-
-            expected = [(b'GREET', "world"), (b'AGREET', "world")]
-
-            async with stream.enumerate(repeat_func_eof(cmd_pipe.recv, b'$TERM')).stream() as streamer:
-                i = -1
-                async for i, cmd in streamer:
-                    assert cmd == expected[i]
-                    cmd, *payload = cmd
-
-                    if cmd == b'GREET':
-                        msg, = payload
-                        await cmd_pipe.send(f"{self.greeting} {msg}")
-                    elif cmd == b'AGREET':
-                        msg, = payload
-                        await evt_pipe.send((b'AGREET', f"{self.greeting} {msg}"))
-                assert i == len(expected) - 1
-            await evt_pipe.send((b'IGNORED',))
-
-    with assertImmediate():
-        with assert_actor_cleanup(MyActor("hello")) as a:
-            async with a:
-                assert await a.greet("world") == "hello world"
-                await a.agreet("world")
-                assert await a.evt_pipe.recv() == (b'AGREET', "hello world")
-
-
-@pytest.mark.asyncio
-async def test_faulty_actor():
-    class FaultyActor(Actor):
-        async def run(self, cmd_pipe, evt_pipe):
-            pass
-
-    with assertImmediate():
-        with pytest.raises(ActorException):
-            with assert_actor_cleanup(FaultyActor()) as a:
-                async with a:
-                    await a
-
-
-@pytest.mark.asyncio
-async def test_failing_actor():
-    class MyException(Exception):
-        pass
-
-    class FailingActor(Actor):
-        async def run(self, cmd_pipe, evt_pipe):
-            await evt_pipe.send(b'$START')
-            raise MyException()
-
-    with assertImmediate():
-        with pytest.raises(MyException):
-            with assert_actor_cleanup(FailingActor()) as a:
-                async with a:
-                    await a
-
-
-@pytest.mark.asyncio
-async def test_init_failing_actor():
-    class MyException(Exception):
-        pass
-
-    class InitFailingActor(Actor):
-        async def run(self, cmd_pipe, evt_pipe):
-            raise MyException()
-
-    with assertImmediate():
-        with pytest.raises(MyException):
-            with assert_actor_cleanup(InitFailingActor()) as a:
-                async with a:
-                    await a
-
-
-@pytest.mark.asyncio
-async def test_actor_consume_term():
-    class FailingActor(Actor):
-        async def run(self, cmd_pipe, evt_pipe):
-            await evt_pipe.send(b'$START')
-
-    with assertImmediate():
-        with assert_actor_cleanup(FailingActor()) as a:
-            async with a:
-                assert await a.evt_pipe.recv() == b'$TERM'
-
-
-@pytest.mark.asyncio
-async def test_actor_stop_early():
-    class FailingActor(Actor):
-        async def run(self, cmd_pipe, evt_pipe):
-            await evt_pipe.send(b'$START')
-            return "foo"
-
-    with assertImmediate():
-        with assert_actor_cleanup(FailingActor()) as a:
-            async with a:
-                assert await a.stop() == "foo"
-
-
-@pytest.mark.asyncio
-async def test_actor_receive_after_term():
-    class AfterTerminationActor(Actor):
-        async def run(self, cmd_pipe, evt_pipe):
-            await evt_pipe.send(b'$START')
-            assert await cmd_pipe.recv() == b'$TERM'
-            await evt_pipe.send(b'AFTER_TERM')
-            return "foo"
-
-    with assertImmediate():
-        with assert_actor_cleanup(AfterTerminationActor()) as a:
-            async with a:
-                assert await a.stop(block=False) is None
-                assert await a.evt_pipe.recv() == b'AFTER_TERM'
-                assert await a == "foo"
+            stream_from_queue(queue, EOF, use_is=True) | pipe.action(put_next))
--- tests/test_zmq.py
+++ tests/test_zmq.py
@@ -2,34 +2,13 @@ import pytest
 from hedgehog.utils.test_utils import event_loop, zmq_ctx, zmq_aio_ctx, assertTimeout, assertImmediate
 
 import asyncio
-import time
 import zmq
 
-from hedgehog.utils.zmq.pipe import pipe, extended_pipe
-from hedgehog.utils.zmq.actor import Actor, CommandRegistry
 from hedgehog.utils.zmq.async_socket import Socket
-from hedgehog.utils.zmq.timer import Timer
 
 
 # Pytest fixtures
 event_loop, zmq_ctx, zmq_aio_ctx
-
-
-class TestPipe(object):
-    def test_pipe(self, zmq_ctx):
-        a, b = pipe(zmq_ctx, endpoint='inproc://endpoint')
-        with a, b:
-            a.signal()
-            b.wait()
-
-    def test_extended_pipe(self, zmq_ctx):
-        a, b = extended_pipe(zmq_ctx, endpoint='inproc://endpoint')
-        with a, b:
-            obj = object()
-            a.push(obj)
-            a.signal()
-            b.wait()
-            assert b.pop() is obj
 
 
 class TestAsyncSocket(object):
@@ -46,111 +25,3 @@ class TestAsyncSocket(object):
                 await a.signal()
                 await task
 
-
-@pytest.mark.skip
-class TestActor(object):
-    def test_actor_termination(self, zmq_ctx):
-        def task(ctx, cmd_pipe, evt_pipe):
-            evt_pipe.signal()
-
-            cmd_pipe.recv_expect(b'do')
-
-        actor = Actor(zmq_ctx, task)
-
-        actor.cmd_pipe.send(b'do')
-
-        # await the actor terminating
-        actor.evt_pipe.recv_expect(b'$TERM')
-
-    def test_actor_destruction(self, zmq_ctx):
-        def task(ctx, cmd_pipe, evt_pipe):
-            evt_pipe.signal()
-            cmd_pipe.recv_expect(b'do')
-            cmd_pipe.recv_expect(b'$TERM')
-
-        actor = Actor(zmq_ctx, task)
-
-        actor.cmd_pipe.send(b'do')
-
-        # this triggers and awaits actor termination
-        actor.destroy()
-
-    def test_actor_destruction_event(self, zmq_ctx):
-        def task(ctx, cmd_pipe, evt_pipe):
-            evt_pipe.signal()
-            cmd_pipe.recv_expect(b'$TERM')
-            evt_pipe.send(b'event')
-            evt_pipe.recv_expect(b'reply')
-
-        actor = Actor(zmq_ctx, task)
-
-        # this triggers actor termination
-        actor.destroy(block=False)
-        actor.evt_pipe.recv_expect(b'event')
-        actor.evt_pipe.send(b'reply')
-        actor.evt_pipe.recv_expect(b'$TERM')
-
-    def test_command_registry(self):
-        registry = CommandRegistry()
-
-        def handler(payload):
-            assert payload == b'payload'
-
-        registry.register(b'test', handler)
-
-        registry.handle((b'test', b'payload'))
-
-    def test_command_registry_decorator(self):
-        registry = CommandRegistry()
-
-        @registry.command(b'test')
-        def handler(payload):
-            assert payload == b'payload'
-
-        registry.handle((b'test', b'payload'))
-
-
-class TestTimer(object):
-    def test_order(self, zmq_ctx):
-        with Timer(zmq_ctx) as timer:
-            a = timer.register(0.010, "a")
-            time.sleep(0.015)  # 0: a, 10: a; time=15
-            b = timer.register(0.010, "b")
-            time.sleep(0.010)  # 15: b, 20: a, 25: b; time=25
-            timer.unregister(a)
-            c = timer.register(0.005, "c", repeat=False)
-            time.sleep(0.015)  # 30:c, 35:b; time=40
-            timer.unregister(b)
-            time.sleep(0.010)  # time=50
-
-            timers = [a, a, b, a, b, c, b]
-
-            events = timer.evt_pipe.poll(0)
-            while events & zmq.POLLIN:
-                timer.evt_pipe.recv_expect(b'TIMER')
-                then, t = timer.evt_pipe.pop()
-                assert t is timers.pop(0)
-                events = timer.evt_pipe.poll(0)
-            assert len(timers) == 0
-
-    @pytest.mark.skip
-    def test_load(self, zmq_ctx):
-        with Timer(zmq_ctx) as timer:
-            ts = [timer.register(0.01, id) for id in range(100)]
-            time.sleep(0.015)
-            for t in ts:
-                timer.unregister(t)
-
-            timers = [0 for t in ts]
-
-            events = timer.evt_pipe.poll(0)
-            while events & zmq.POLLIN:
-                timer.evt_pipe.recv_expect(b'TIMER')
-                then, t = timer.evt_pipe.pop()
-                timers[t.aux] += 1
-                events = timer.evt_pipe.poll(0)
-            assert timers == [2 for t in ts]
-
-    def test_terminate(self, zmq_ctx):
-        with Timer(zmq_ctx) as timer:
-            timer.register(0.1)
