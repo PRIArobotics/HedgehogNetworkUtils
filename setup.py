6249a7201d5ae637eb425dab34e417e767187fd7
SillyFreak <sillyfreak.ck@gmail.com>
2018-10-29 11:56:40 +0100

    update project configuration files

--- setup.py
+++ setup.py
@@ -51,7 +51,8 @@ setup(
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['invoke', 'pytest', 'pytest-runner', 'pytest-asyncio', 'pytest-cov'],
+        'dev': ['invoke',
+                'pytest', 'pytest-runner', 'pytest-asyncio', 'pytest-cov', 'pytest-timeout', 'mypy'],
         'protobuf': ['protobuf'],
         'zmq': ['pyzmq'],
     },
